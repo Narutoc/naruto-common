504654f4ba5f1e191062f03bc9070c342c24c579
Narutoc <1187340828@qq.com>
2022-05-01 09:38:30 +0800

    fix#000004 update common

--- src/main/java/com/local/naruto/config/RedisConfig.java
+++ src/main/java/com/local/naruto/config/RedisConfig.java
@@ -1,7 +1,82 @@
 package com.local.naruto.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import java.lang.reflect.Method;
+import java.time.Duration;
 
 @Configuration
-public class RedisConfig {
+@Conditional(CacheTypeCondition.class)
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.application.name}")
+    private String prefix;
+
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+
+    @Bean
+    @Primary
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redis = RedisCacheConfiguration.defaultCacheConfig();
+        redis.entryTtl(Duration.ofSeconds(86400));
+        redis.computePrefixWith(new CacheKeyPrefix() {
+            private final String delimiter = ":";
+
+            @Override
+            public String compute(String cacheName) {
+                return prefix.concat(delimiter).concat(cacheName);
+            }
+        });
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(redis).build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redis) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redis);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
+            DefaultTyping.NON_FINAL, As.WRAPPER_ARRAY);
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(redisTemplate.getKeySerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
 }
--- src/main/java/com/local/naruto/config/RestTemplateConfiguration.java
+++ src/main/java/com/local/naruto/config/RestTemplateConfiguration.java
@@ -1,4 +1,18 @@
 package com.local.naruto.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
 public class RestTemplateConfiguration {
+
+    private static final int CONNECT_TIME_OUT = 5000;
+    private static final int READ_TIME_OUT = 40000;
+    private static final int MAX_POOL_SIZE = 100;
+
+    @Bean
+    public RedisTemplate redisTemplate() {
+        return new RedisTemplate();
+    }
 }
