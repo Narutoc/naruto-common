fcc2eeec675c9273b327fd154fab2f1d77ba3fe7
Narutoc <1187340828@qq.com>
2022-05-02 21:16:08 +0800

    fix#000004 fix redis

--- src/main/java/com/local/naruto/config/CacheTypeCondition.java
+++ src/main/java/com/local/naruto/config/CacheTypeCondition.java
@@ -1,14 +1,14 @@
-//package com.local.naruto.config;
-//
-//import org.springframework.context.annotation.Condition;
-//import org.springframework.context.annotation.ConditionContext;
-//import org.springframework.core.type.AnnotatedTypeMetadata;
-//
-//public class CacheTypeCondition implements Condition {
-//
-//    @Override
-//    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-//        String cacheType = context.getEnvironment().getProperty("spring.cache.type");
-//        return cacheType != null && cacheType.equals("redis");
-//    }
-//}
+package com.local.naruto.config;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class CacheTypeCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        String cacheType = context.getEnvironment().getProperty("spring.cache.type");
+        return cacheType != null && cacheType.equals("redis");
+    }
+}
--- src/main/java/com/local/naruto/config/RedisConfig.java
+++ src/main/java/com/local/naruto/config/RedisConfig.java
@@ -1,72 +1,72 @@
-//package com.local.naruto.config;
-//
-//import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-//import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
-//import com.fasterxml.jackson.annotation.PropertyAccessor;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
-//import java.time.Duration;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.cache.CacheManager;
-//import org.springframework.cache.annotation.CachingConfigurerSupport;
-//import org.springframework.cache.interceptor.KeyGenerator;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Conditional;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.data.redis.cache.RedisCacheConfiguration;
-//import org.springframework.data.redis.cache.RedisCacheManager;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@Configuration
-//@Conditional(CacheTypeCondition.class)
-//public class RedisConfig extends CachingConfigurerSupport {
-//
-//    @Value("${spring.application.name}")
-//    private String prefix;
-//
-//    @Override
-//    @Bean
-//    public KeyGenerator keyGenerator() {
-//        return (target, method, params) -> {
-//            StringBuilder sb = new StringBuilder();
-//            sb.append(target.getClass().getName());
-//            sb.append(method.getName());
-//            for (Object obj : params) {
-//                sb.append(obj.toString());
-//            }
-//            return sb.toString();
-//        };
-//    }
-//
-//    @Bean
-//    @Primary
-//    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-//        RedisCacheConfiguration redis = RedisCacheConfiguration.defaultCacheConfig();
-//        redis.entryTtl(Duration.ofSeconds(86400));
-//        redis.computePrefixWith(cacheName -> {
-//            String delimiter = ":";
-//            return prefix.concat(delimiter).concat(cacheName);
-//        });
-//        return RedisCacheManager.builder(connectionFactory).cacheDefaults(redis).build();
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redis) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(redis);
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
-//        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
-//            DefaultTyping.NON_FINAL, As.WRAPPER_ARRAY);
-//        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
-//        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setHashKeySerializer(redisTemplate.getKeySerializer());
-//        redisTemplate.afterPropertiesSet();
-//        return redisTemplate;
-//    }
-//}
+package com.local.naruto.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping;
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Conditional(CacheTypeCondition.class)
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Value("${spring.application.name}")
+    private String prefix;
+
+    @Override
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return (target, method, params) -> {
+            StringBuilder sb = new StringBuilder();
+            sb.append(target.getClass().getName());
+            sb.append(method.getName());
+            for (Object obj : params) {
+                sb.append(obj.toString());
+            }
+            return sb.toString();
+        };
+    }
+
+    @Bean
+    @Primary
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redis = RedisCacheConfiguration.defaultCacheConfig();
+        redis.entryTtl(Duration.ofSeconds(86400));
+        redis.computePrefixWith(cacheName -> {
+            String delimiter = ":";
+            return prefix.concat(delimiter).concat(cacheName);
+        });
+        return RedisCacheManager.builder(connectionFactory).cacheDefaults(redis).build();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redis) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redis);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.ANY);
+        objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(),
+            DefaultTyping.NON_FINAL, As.WRAPPER_ARRAY);
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(redisTemplate.getKeySerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
